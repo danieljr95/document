95cb27885a3224a101a4b23d8596af7c1345f9db
danieljr <djaraolivera@gmail.com>
2024-02-24 23:54:42 -0500

    Añadida clase A con atributos foo, bar, y javascript

---
+++ a.js
@@ -0,0 +1,10 @@
+// A.js
+class A {
+    constructor(foo, bar, javascript) {
+        this.foo = foo;
+        this.bar = bar;
+        this.javascript = javascript;
+    }
+}
+
+module.exports = A;
